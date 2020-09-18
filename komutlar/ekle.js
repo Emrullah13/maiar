b7ae50e3a21bf0367e52ec6b8e3c5ab2095546c3
Emrullah13 <61323677+Emrullah13@users.noreply.github.com>
2020-09-18 16:40:22 +0300

    Update ekle.js

--- komutlar/ekle.js
+++ komutlar/ekle.js
@@ -4,12 +4,9 @@ exports.run = async (bot, message, args) => {
 
   let user = client.users.get(args.slice(0).join(' '));
   let nesne = args[0]
-   if (!nesne) return message.reply('Show link nerede?')
   
 
- message.client.channels.get('712068971922456696').send(` ${message.author} adlı kişi <a:biciku:714486266347126898> ${nesne} linki ile uptime etti`)
-
-
+ message.client.channels.get('712068971922456696').send(` ${message.author} adlı kişi  ${nesne} linki ile uptime etti`)
 
   
 }
